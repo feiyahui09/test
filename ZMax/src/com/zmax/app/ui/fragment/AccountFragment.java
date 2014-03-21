813934c205d2e80edd70dd75fa3dac47cd8b253b
feiyahui000 <feiyahui000@gmail.com>
2014-03-21 16:52:19 +0800

    设置，账户登录 ui初步

--- ZMax/src/com/zmax/app/ui/fragment/AccountFragment.java
+++ ZMax/src/com/zmax/app/ui/fragment/AccountFragment.java
@@ -4,39 +4,74 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.zmax.app.R;
 
-public class AccountFragment extends Fragment {
-	
-	private int mColorRes = -1;
-	
-	public AccountFragment() { 
+public class AccountFragment extends Fragment implements OnClickListener {
+
+	private Button btn_confirm;
+	private EditText et_account, et_password;
+	private TextView tv_forget_pw, tv_regist;
+
+	public AccountFragment() {
 		this(R.color.white);
 	}
-	
+
 	public AccountFragment(int colorRes) {
-		mColorRes = colorRes;
 		setRetainInstance(true);
 	}
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		if (savedInstanceState != null)
-			mColorRes = savedInstanceState.getInt("mColorRes");
-		int color = getResources().getColor(mColorRes);
-		// construct the RelativeLayout
-		RelativeLayout v = new RelativeLayout(getActivity());
-		v.setBackgroundColor(color);		
-		return v;
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.account_login, null);
+		btn_confirm = (Button) view.findViewById(R.id.btn_confirm);
+		btn_confirm.setOnClickListener(this);
+		et_account = (EditText) view.findViewById(R.id.et_account);
+		et_account.setOnClickListener(this);
+		et_password = (EditText) view.findViewById(R.id.et_password);
+		et_password.setOnClickListener(this);
+		tv_forget_pw = (TextView) view.findViewById(R.id.tv_forget_pw);
+		tv_forget_pw.setOnClickListener(this);
+		tv_regist = (TextView) view.findViewById(R.id.tv_regist);
+		tv_regist.setOnClickListener(this);
+
+		return view;
 	}
-	
+
 	@Override
 	public void onSaveInstanceState(Bundle outState) {
 		super.onSaveInstanceState(outState);
-		outState.putInt("mColorRes", mColorRes);
 	}
-	
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+
+		case R.id.tv_regist:
+
+			break;
+		case R.id.tv_forget_pw:
+
+			break;
+		case R.id.et_password:
+
+			break;
+		case R.id.et_account:
+
+			break;
+		case R.id.btn_confirm:
+
+			break;
+
+		default:
+			break;
+		}
+
+	}
 }
--- ZMax/src/com/zmax/app/ui/fragment/SettingFragment.java
+++ ZMax/src/com/zmax/app/ui/fragment/SettingFragment.java
@@ -1,48 +1,85 @@
 package com.zmax.app.ui.fragment;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
+import android.widget.Button;
 
 import com.zmax.app.R;
-import com.zmax.app.ui.HotelDatePickActivity;
 
-public class SettingFragment extends Fragment {
+public class SettingFragment extends Fragment implements OnClickListener {
 
-	private int mColorRes = -1;
+	private Button btn_feedback, btn_check_update, btn_welcome,
+			btn_user_regulation, btn_play_zmax, btn_member, btn_about;
 
 	public SettingFragment() {
 		this(R.color.white);
 	}
 
 	public SettingFragment(int colorRes) {
-		mColorRes = colorRes;
 		setRetainInstance(true);
 	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		if (savedInstanceState != null)
-			mColorRes = savedInstanceState.getInt("mColorRes");
-		int color = getResources().getColor(mColorRes);
-		// construct the RelativeLayout
-		RelativeLayout v = new RelativeLayout(getActivity());
-		v.setBackgroundColor(color);
-		Intent intent = new Intent();
-		intent.setClass(getActivity(), HotelDatePickActivity.class);
-		getActivity().startActivity(intent);
-		return v;
+		View view = inflater.inflate(R.layout.setting, null);
+		btn_feedback = (Button) view.findViewById(R.id.btn_feedback);
+		btn_feedback.setOnClickListener(this);
+		btn_check_update = (Button) view.findViewById(R.id.btn_check_update);
+		btn_check_update.setOnClickListener(this);
+		btn_welcome = (Button) view.findViewById(R.id.btn_welcome);
+		btn_welcome.setOnClickListener(this);
+		btn_user_regulation = (Button) view
+				.findViewById(R.id.btn_user_regulation);
+		btn_user_regulation.setOnClickListener(this);
+		btn_play_zmax = (Button) view.findViewById(R.id.btn_play_zmax);
+		btn_play_zmax.setOnClickListener(this);
+		btn_member = (Button) view.findViewById(R.id.btn_member);
+		btn_member.setOnClickListener(this);
+		btn_about = (Button) view.findViewById(R.id.btn_about);
+		btn_about.setOnClickListener(this);
+
+		return view;
 	}
 
 	@Override
 	public void onSaveInstanceState(Bundle outState) {
 		super.onSaveInstanceState(outState);
-		outState.putInt("mColorRes", mColorRes);
 	}
 
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+
+		case R.id.btn_feedback:
+
+			break;
+		case R.id.btn_check_update:
+
+			break;
+		case R.id.btn_welcome:
+
+			break;
+		case R.id.btn_user_regulation:
+
+			break;
+		case R.id.btn_play_zmax:
+
+			break;
+		case R.id.btn_member:
+
+			break;
+		case R.id.btn_about:
+
+			break;
+
+		default:
+			break;
+		}
+
+	}
 }
