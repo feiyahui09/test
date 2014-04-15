fa16cc2d1935124762ee015b0701d51a3a92caff
feiyahui000 <feiyahui000@gmail.com>
2014-04-15 16:07:50 +0800

    1.活动详情UI调整； 2.listview隐藏scrollbar；

--- ZMax/src/com/zmax/app/ui/fragment/ActDetailSecondFragment.java
+++ ZMax/src/com/zmax/app/ui/fragment/ActDetailSecondFragment.java
@@ -7,20 +7,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
 
 import com.zmax.app.R;
 import com.zmax.app.adapter.ActDescptionListAdapter;
-import com.zmax.app.adapter.ActListAdapter;
 import com.zmax.app.model.ActDetailContent;
-import com.zmax.app.ui.MainActivity;
 import com.zmax.app.ui.ActDetailActivity.RefreshDataCallBack;
-import com.zmax.app.utils.Constant;
-import com.zmax.app.widget.XListView;
-import com.zmax.app.widget.XListView.IXListViewListener;
 
-public class ActDetailSecondFragment extends Fragment implements IXListViewListener, OnItemClickListener, RefreshDataCallBack {
+public class ActDetailSecondFragment extends Fragment implements OnItemClickListener, RefreshDataCallBack {
 	
-	protected XListView listview;
+	protected ListView listview;
 	protected View view;
 	
 	private ActDescptionListAdapter adapter;
@@ -36,13 +32,9 @@ public class ActDetailSecondFragment extends Fragment implements IXListViewListe
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		view = inflater.inflate(R.layout.act_detail_second, null);
-		listview = (XListView) view.findViewById(R.id.list_view);
-		listview.setPullLoadEnable(false);
-		listview.setPullRefreshEnable(false);
-		
+		listview = (ListView) view.findViewById(R.id.list_view);
 		adapter = new ActDescptionListAdapter(getActivity());
-		
-//		listview.setOnItemClickListener(this);
+		// listview.setOnItemClickListener(this);
 		
 		return view;
 	}
@@ -50,23 +42,6 @@ public class ActDetailSecondFragment extends Fragment implements IXListViewListe
 	@Override
 	public void onSaveInstanceState(Bundle outState) {
 		super.onSaveInstanceState(outState);
-	}
-	
-	@Override
-	public void onRefresh() {
-		
-	}
-	
-	@Override
-	public void onLoadMore() {
-		// adapter.appendToList(Constant.getFalseData(false));
-		onLoad();
-	}
-	
-	protected void onLoad() {
-		listview.stopRefresh();
-		listview.stopLoadMore();
-		listview.setRefreshTime("刚刚");
 	}
 	
 	@Override
