e4db68e9c7d4360238e270c0139675724c205b17
feiyahui000 <feiyahui000@gmail.com>
2014-05-06 20:47:07 +0800

    try-cathch for "Can not perform this action after onSaveInstanceState"

--- dialog_libs/src/eu/inmite/android/lib/dialogs/BaseDialogBuilder.java
+++ dialog_libs/src/eu/inmite/android/lib/dialogs/BaseDialogBuilder.java
@@ -7,38 +7,39 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 
 /**
- * Internal base builder that holds common values for all dialog fragment builders.
- *
+ * Internal base builder that holds common values for all dialog fragment
+ * builders.
+ * 
  * @author Tomas Vondracek
  */
 abstract class BaseDialogBuilder<T extends BaseDialogBuilder<T>> {
-
+	
 	public final static String ARG_REQUEST_CODE = "request_code";
 	public final static String ARG_CANCELABLE_ON_TOUCH_OUTSIDE = "cancelable_oto";
 	public final static String DEFAULT_TAG = "simple_dialog";
 	public final static int DEFAULT_REQUEST_CODE = -42;
-
+	
 	protected final Context mContext;
 	protected final FragmentManager mFragmentManager;
 	protected final Class<? extends BaseDialogFragment> mClass;
-
+	
 	private Fragment mTargetFragment;
 	private boolean mCancelable = true;
 	private boolean mCancelableOnTouchOutside = true;
-
+	
 	private String mTag = DEFAULT_TAG;
 	private int mRequestCode = DEFAULT_REQUEST_CODE;
-
+	
 	public BaseDialogBuilder(Context context, FragmentManager fragmentManager, Class<? extends BaseDialogFragment> clazz) {
 		mFragmentManager = fragmentManager;
 		mContext = context.getApplicationContext();
 		mClass = clazz;
 	}
-
+	
 	protected abstract T self();
-
+	
 	protected abstract Bundle prepareArguments();
-
+	
 	public T setCancelable(boolean cancelable) {
 		mCancelable = cancelable;
 		return self();
@@ -51,38 +52,43 @@ abstract class BaseDialogBuilder<T extends BaseDialogBuilder<T>> {
 		}
 		return self();
 	}
-
+	
 	public T setTargetFragment(Fragment fragment, int requestCode) {
 		mTargetFragment = fragment;
 		mRequestCode = requestCode;
 		return self();
 	}
-
+	
 	public T setRequestCode(int requestCode) {
 		mRequestCode = requestCode;
 		return self();
 	}
-
+	
 	public T setTag(String tag) {
 		mTag = tag;
 		return self();
 	}
-
-
+	
 	public DialogFragment show() {
 		final Bundle args = prepareArguments();
-
+		
 		final BaseDialogFragment fragment = (BaseDialogFragment) Fragment.instantiate(mContext, mClass.getName(), args);
-	
+		
 		args.putBoolean(ARG_CANCELABLE_ON_TOUCH_OUTSIDE, mCancelableOnTouchOutside);
 		
 		if (mTargetFragment != null) {
 			fragment.setTargetFragment(mTargetFragment, mRequestCode);
-		} else {
+		}
+		else {
 			args.putInt(ARG_REQUEST_CODE, mRequestCode);
 		}
 		fragment.setCancelable(mCancelable);
-		fragment.show(mFragmentManager, mTag);
+		try {
+			fragment.show(mFragmentManager, mTag);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 		return fragment;
 	}
